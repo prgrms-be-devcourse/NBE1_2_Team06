25be3e7738ee6ccc09ddf27ae2f2bc0c159f56a9
Ban-gilhyeon <bbgiloo98@gmail.com>
2024-09-26 13:01:54 +0900

    feat : 공통 필수 엔티티 추가 #19
    User 엔티티 테이블 할당
    EmergencyRoom 엔티티 추가

--- ea-domain/src/main/java/com/nbe2/domain/user/User.java
+++ ea-domain/src/main/java/com/nbe2/domain/user/User.java
@@ -1,22 +1,34 @@
 package com.nbe2.domain.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import com.nbe2.domain.global.BaseTimeEntity;
+
 @Entity
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+@Table(name = "users")
+public class User extends BaseTimeEntity {
 
-    @Id private String id;
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    private String nickname;
+    private String name;
 
     private String email;
+
+    private String password;
 }
