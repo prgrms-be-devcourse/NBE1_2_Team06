ebf9836972330cf754e716d33e560f2b5126a9f1
강지원 <kangji149@naver.com>
2024-09-27 18:34:29 +0900

    refactor : 파일 저장 위치 상대경로로 설정 #25

--- ea-application/app-api/src/main/java/com/nbe2/api/global/util/FileUtils.java
+++ ea-application/app-api/src/main/java/com/nbe2/api/global/util/FileUtils.java
@@ -3,23 +3,37 @@ package com.nbe2.api.global.util;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.nbe2.api.global.exception.FileNotFoundException;
 import com.nbe2.domain.file.FileMetaData;
 
-@Component
 public class FileUtils {
 
-    private static String UPLOAD_PATH;
+    private static final String BASE_PATH = getBasePath();
+    private static final String RELATIVE_UPLOAD_PATH = "files/";
+    private static final String UPLOAD_PATH = BASE_PATH + RELATIVE_UPLOAD_PATH;
 
-    @Value("${file.upload-path}")
-    public void setUploadPath(String uploadPath) {
-        UPLOAD_PATH = uploadPath;
+    private static String getBasePath() {
+        try {
+            return Paths.get(
+                                    FileUtils.class
+                                            .getProtectionDomain()
+                                            .getCodeSource()
+                                            .getLocation()
+                                            .toURI())
+                            .getParent()
+                            .getParent()
+                            .getParent()
+                            .getParent()
+                            .toString()
+                    + "/";
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot determine base path.", e);
+        }
     }
 
     public static FileMetaData saveFile(MultipartFile uploadFile) {
