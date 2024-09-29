41f07ed995c35ee1ade73f7f54c3f91c2f490ec1
Ban-gilhyeon <bbgiloo98@gmail.com>
2024-09-29 23:31:26 +0900

    Auto stash before rebase of "feature/notices"

--- ea-application/app-api/src/main/java/com/nbe2/api/notice/dto/NoticeUpdateReqeust.java
+++ ea-application/app-api/src/main/java/com/nbe2/api/notice/dto/NoticeUpdateReqeust.java
@@ -1,14 +1,11 @@
 package com.nbe2.api.notice.dto;
 
-import com.nbe2.domain.notice.NoticeInfo;
-import com.nbe2.domain.notice.NoticeUpdateInfo;
 import java.util.Optional;
 
-public record NoticeUpdateReqeust(
-		String title,
-		String content
-) {
-	public NoticeUpdateInfo toNoticeUpdateInfo() {
-		return new NoticeUpdateInfo(Optional.ofNullable(title), Optional.ofNullable(content));
-	}
+import com.nbe2.domain.notice.NoticeUpdateInfo;
+
+public record NoticeUpdateReqeust(String title, String content) {
+    public NoticeUpdateInfo toNoticeUpdateInfo() {
+        return new NoticeUpdateInfo(Optional.ofNullable(title), Optional.ofNullable(content));
+    }
 }
