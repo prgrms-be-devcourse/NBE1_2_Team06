a968f38e316d1875ee60f826d7941d9ab2294c06
Ban-gilhyeon <bbgiloo98@gmail.com>
2024-10-07 09:54:14 +0900

    refactor : 안쓰는 필드 삭제 #55

--- ea-domain/src/main/java/com/nbe2/domain/review/ReviewReader.java
+++ ea-domain/src/main/java/com/nbe2/domain/review/ReviewReader.java
@@ -9,14 +9,11 @@ import lombok.RequiredArgsConstructor;
 
 import com.nbe2.common.dto.PageResult;
 import com.nbe2.domain.review.exception.ReviewNotFoundException;
-import com.nbe2.domain.user.UserReader;
 
 @Component
 @RequiredArgsConstructor
 public class ReviewReader {
     private final ReviewRepository reviewRepository;
-    private final Validator validator;
-    private final UserReader userReader;
 
     public PageResult<ReviewReadInfo> readAll(Pageable pageable, Long emergencyRoomId) {
         Page<ReviewReadInfo> readReviews =
