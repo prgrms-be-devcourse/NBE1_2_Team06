08a2956d060ddb26f592c8db9c429e01caff8982
s0o0bn <0921soobin@gmail.com>
2024-10-13 20:24:25 +0900

    feat : 알림 삭제 스케줄러에 shedlock 적용 #81

---
+++ ea-infra/src/main/java/com/nbe2/infra/notification/config/SchedulerConfig.java
@@ -0,0 +1,33 @@
+package com.nbe2.infra.notification.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+
+@Configuration
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT5M")
+public class SchedulerConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        scheduler.setThreadNamePrefix("scheduled-task-pool-");
+        scheduler.initialize();
+        taskRegistrar.setTaskScheduler(scheduler);
+    }
+
+    @Bean
+    public LockProvider lockProvider(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisLockProvider(redisConnectionFactory);
+    }
+}
--- ea-infra/src/main/java/com/nbe2/infra/notification/scheduler/NotificationScheduler.java
+++ ea-infra/src/main/java/com/nbe2/infra/notification/scheduler/NotificationScheduler.java
@@ -5,18 +5,24 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import com.nbe2.domain.notification.NotificationDeleter;
 
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class NotificationScheduler {
 
     private final NotificationDeleter notificationDeleter;
 
     @Transactional
     @Scheduled(cron = "0 0 0 * * *")
+    @SchedulerLock(name = "notification", lockAtLeastFor = "PT2S", lockAtMostFor = "PT2S")
     public void deleteOutdatedNotifications() {
+        log.info("Deleting outdated notifications");
         notificationDeleter.deleteOutdatedNotifications();
     }
 }
