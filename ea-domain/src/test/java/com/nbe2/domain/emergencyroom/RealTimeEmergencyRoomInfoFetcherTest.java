5194cd016acb3e1b8335c405ee78e8a6d49b2da3
Ban-gilhyeon <bbgiloo98@gmail.com>
2024-10-17 09:46:05 +0900

    refactor : 응급실 상세조회 PR 수정에 따른 수정으로 테스트 코드 수정#88

--- ea-domain/src/test/java/com/nbe2/domain/emergencyroom/RealTimeEmergencyRoomInfoFetcherTest.java
+++ ea-domain/src/test/java/com/nbe2/domain/emergencyroom/RealTimeEmergencyRoomInfoFetcherTest.java
@@ -22,24 +22,27 @@ class RealTimeEmergencyRoomInfoFetcherTest {
 
     @Mock private RealTimeEmergencyRoomInfoCacheManager cacheManager;
 
+    @Mock private CoordinateToRegionConverter coordinateToRegionConverter;
+
     @Nested
     @DisplayName("실시간 응급실 정보를 조회한다.")
     class FetchRealTimeInfoTest {
 
         @Test
         @DisplayName("유효한 지역을 전달하면 실시간 응급실 정보를 반환하고 캐싱한다.")
-        void givenRegion_whenFetchRealTimeInfo_thenReturnAndCacheInfo() {
+        void givenCoordate_whenConvertRegion_FetchRealTimeInfo_thenReturnAndCacheInfo() {
             // given
-            Region region = EmergencyRoomFixture.getRegion();
+            Coordinate coordinate = EmergencyRoomFixture.COORDINATE;
             List<RealTimeEmergencyRoomInfo> expectedInfo =
                     EmergencyRoomFixture.createRealTimeInfoList();
 
             // when
+            Region region = coordinateToRegionConverter.convert(coordinate);
             when(realTimeClient.getRealTimeInfo(region)).thenReturn(expectedInfo);
 
             // then
             List<RealTimeEmergencyRoomInfo> actualInfo =
-                    realTimeEmergencyRoomInfoFetcher.fetch(region);
+                    realTimeEmergencyRoomInfoFetcher.fetch(coordinate);
 
             assertEquals(expectedInfo, actualInfo);
             verify(realTimeClient, times(1)).getRealTimeInfo(region);
@@ -48,18 +51,19 @@ class RealTimeEmergencyRoomInfoFetcherTest {
 
         @Test
         @DisplayName("캐시 매니저가 호출되면 캐싱 로직이 동작한다.")
-        void givenRealTimeInfo_whenCacheManagerCalled_thenCacheInfo() {
+        void givenCoordinate_RealTimeInfo_whenConvertRegion_CacheManagerCalled_thenCacheInfo() {
             // given
-            Region region = EmergencyRoomFixture.getRegion();
+            Coordinate coordinate = EmergencyRoomFixture.COORDINATE;
             List<RealTimeEmergencyRoomInfo> realTimeInfo =
                     EmergencyRoomFixture.createRealTimeInfoList();
 
             // when
+            Region region = coordinateToRegionConverter.convert(coordinate);
             when(realTimeClient.getRealTimeInfo(region)).thenReturn(realTimeInfo);
             doNothing().when(cacheManager).cache(realTimeInfo);
 
             // then
-            realTimeEmergencyRoomInfoFetcher.fetch(region);
+            realTimeEmergencyRoomInfoFetcher.fetch(coordinate);
             verify(cacheManager, times(1)).cache(realTimeInfo);
         }
     }
